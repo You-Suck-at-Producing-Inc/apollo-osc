f2d1a46ab2f6ca899d085a7ced3351ddd0abe72d
Felipe Campos <fmrcampos@berkeley.edu>
2024-08-02 19:21:50 -0700

    partial tree enumeration [hacky merge - pre-multi-category collective tree aggregation]

--- abletonosc/browser.py
+++ abletonosc/browser.py
@@ -18,13 +18,8 @@ class BrowserHandler(AbletonOSCHandler):
     def init_api(self):
         temp_dir = '/tmp'
         self.browser = Live.Application.get_application().browser
-        # self.chain = Live.Application.get_application().chain
 
-        methods = [
-            "load_item",
-            "preview_item",
-            "stop_preview"
-        ]
+        # methods: load_item, preview_item, stop_preview
         properties_r = [
             "audio_effects",
             "clips",
@@ -37,37 +32,15 @@ class BrowserHandler(AbletonOSCHandler):
             "user_library"
         ]
 
-        # for method in methods:
-        #     self.osc_server.add_handler(f"/live/browser/{method}", partial(self._call_method, self.browser, method))
-
         def get_browser_tree(category: str):
             device_tree = LiveDeviceTree(getattr(self.browser, category))
             temp_file_path = os.path.join(temp_dir, f'ableton_{category}_tree_data.pkl')
-
-            # def traverse(node):
-            #     uris = []
-            #     if len(node.children) == 0:
-            #         uris.append(node.uri)
-            #     else:
-            #         for child in node.children:
-            #             uris.extend(traverse(child))
-            #     return uris
             
             device_tree.save_tree(temp_file_path)
             device_tree_json = device_tree.to_json()
             with open(temp_file_path.replace('.pkl', '.json'), 'w') as f:
                 f.write(json.dumps(device_tree_json))
             return temp_file_path
-        
-            result = [i.uri for i in self.browser.instruments.children]
-            # for i in self.browser.instruments.children:
-            #     print(i)
-            #     print(i.uri)
-            #     print(i.__module__)
-            #     print(i.__dict__)
-            #     print(dir(i))
-            #     print("\n\n\n")
-            return str(result) # iterate over self.browser.instruments.children --> <Browser.BrowserItemVector object at 0x10e4d57e8>
         
         def load_from_browser_tree(name: str, category: Optional[Union[str, List[str]]] = None):
             # if name == "":
@@ -84,50 +57,15 @@ class BrowserHandler(AbletonOSCHandler):
                     self.browser.load_item(browser_item)
                     return browser_item.name
             return False # str((category, name, browser_item, str(device_tree)[:1000]))
-        
-        def get_drum_tree():
-            category = "drums"
-            device_tree = LiveDeviceTree(getattr(self.browser, category))
-            temp_file_path = os.path.join(temp_dir, f'ableton_{category}_tree_data.pkl')
 
-            # def traverse(node):
-            #     uris = []
-            #     if len(node.children) == 0:
-            #         uris.append(node.uri)
-            #     else:
-            #         for child in node.children:
-            #             uris.extend(traverse(child))
-            #     return uris
-            
-            device_tree.save_tree(temp_file_path)
-            return temp_file_path
-
-        def load_from_drum_tree(name: str):
-            live_tree = self.browser.drums
-            device_tree = LiveDeviceTree(live_tree)
-            browser_item = device_tree.find_corresponding_node(live_tree, name)
-            if browser_item is not None:
-                self.browser.load_item(browser_item)
-                return browser_item.name
-            return False # str((category, name, browser_item, str(device_tree)[:1000]))
-        
-        #assert 1 == 2, f"{device_tree.__class__}\n\n{device_tree.get_leaf_nodes()}"
-        #assert 1 == 2, f"{device_tree}\n\n{device_tree.get_leaf_nodes()}\n\n{device_tree.get_nodes_by_parent(['Electric'])}\n\n{device_tree.get_nodes_by_type('instrument')}"
-        
-        #self.browser.preview_item(self.browser.instruments.children[0].children[1].children[0])
-        #assert 1 == 2, f"{dir(self.browser)}\n\n{self.browser.instruments.children[0].children[0].children[0]}\n\n{self.browser.instruments.__module__}\n\n{self.browser.instruments}\n\n{self.browser.instruments.__dict__}\n\n{self.browser.instruments.__dir__()}"
-        #self.osc_server.add_handler("/live/browser/get/instruments", lambda _: ("47",))
-        for prop in properties_r:
-            self.osc_server.add_handler(f"/live/browser/get/{prop}", lambda _: (get_browser_tree(prop),))
-        # self.osc_server.add_handler(f"/live/browser/get/device", lambda _: (get_device_tree(),))
+        # DO NOT CHANGE THIS INTO A FOR LOOP - IT WILL BREAK
+        self.osc_server.add_handler(f"/live/browser/get/instruments", lambda _: (get_browser_tree("instruments"),))
         self.osc_server.add_handler(f"/live/browser/get/audio_effects", lambda _: (get_browser_tree("audio_effects"),))
+        self.osc_server.add_handler(f"/live/browser/get/clips", lambda _: (get_browser_tree("clips"),))
         self.osc_server.add_handler(f"/live/browser/get/drums", lambda _: (get_browser_tree("drums"),))
-        # self.osc_server.add_handler("/live/browser/get/%s" % prop, lambda _: (get_browser_tree,))
-        # for prop in properties_r:
-        #     self.osc_server.add_handler("/live/browser/set/%s" % prop, lambda name: (load_from_browser_tree(prop, name[0]),))
+        self.osc_server.add_handler(f"/live/browser/get/packs", lambda _: (get_browser_tree("packs"),))
+        self.osc_server.add_handler(f"/live/browser/get/samples", lambda _: (get_browser_tree("samples"),))
+        self.osc_server.add_handler(f"/live/browser/get/sounds", lambda _: (get_browser_tree("sounds"),))
+        self.osc_server.add_handler(f"/live/browser/get/user_folders", lambda _: (get_browser_tree("user_folders"),))
+        self.osc_server.add_handler(f"/live/browser/get/user_library", lambda _: (get_browser_tree("user_library"),))
         self.osc_server.add_handler("/live/browser/set/device", lambda name: (load_from_browser_tree(name[0]),)) # general loading TODO: add specified optimized loading (even allowing tree branch passing)
-        # self.osc_server.add_handler("/live/browser/set/audio_effects", lambda name: (load_from_browser_tree("audio_effects", name[0]),))
-        # self.osc_server.add_handler("/live/browser/set/drums", lambda name: (load_from_browser_tree("drums", name[0]),))
-        # self.osc_server.add_handler("/live/browser/set/instruments", lambda _: (get_instruments(),))
-        # for prop in properties_r:
-        #     self.osc_server.add_handler("/live/browser/get/%s" % prop, partial(self._get_property, self.browser, prop))
